f04d3aa28cb44f9de5f8dd5ce86fb00d4dc19cf7
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-06-10 19:29:03 -0700

    added inner HTML and double apendChild

--- kanban.js
+++ kanban.js
@@ -1,13 +1,34 @@
+
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
 
+
+
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+ 
+    // create div to hold list item
+    var taskInfoEl = document.createElement("div");
+    // give it a name 
+    taskInfoEl.className = "task-info"
+    // add HTML content to Div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "<h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl)
+
+    // listItemEl.textContent = taskNameInput;
     tasksToDoEl.appendChild(listItemEl);
 }
 
