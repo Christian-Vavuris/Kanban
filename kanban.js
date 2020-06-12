73ddad0bfbfc6ffd87047f807c2357af4d280af4
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-06-12 10:35:10 -0700

    adding delete function changes

--- kanban.js
+++ kanban.js
@@ -3,6 +3,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content")
 
 var taskFormHandler = function(event) {
 
@@ -27,7 +28,7 @@ var taskFormHandler = function(event) {
     };
 
     createTaskEl(taskDataObj);
-}
+};
 
 
 var createTaskEl = function (taskDataObj){
@@ -46,11 +47,10 @@ var createTaskEl = function (taskDataObj){
         var taskActionsEl = createTaskActions(taskIdCounter);
         listItemEl.appendChild(taskActionsEl);
 
-
         tasksToDoEl.appendChild(listItemEl);
 
         taskIdCounter++;
-}
+};
 
 var createTaskActions = function(taskId) {
     var actionContainerEl = document.createElement("div");
@@ -91,10 +91,25 @@ var createTaskActions = function(taskId) {
     }
 
     return actionContainerEl;
-}
+};
 
+var taskButtonHandler = function (event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        console.log(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+  };
+
+  
 formEl.addEventListener("submit", taskFormHandler);
-
+pageContentEl.addEventListener("click", taskButtonHandler);
 
 
 
